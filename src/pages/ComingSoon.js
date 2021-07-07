5c53a74f0d0ab280f067b6e5508826e64f803f0f
Brighton Ngema <brightonngema@setups-MacBook-Pro.local>
2021-07-07 22:23:03 +0200

    Added assets

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
-import { isMobile } from 'react-device-detect'
+import React, { Component } from 'react';
+import { isMobile } from 'react-device-detect';
 import Logo from "../assets/angelfoundation_logo_orange.png";
+import { Helmet } from "react-helmet";
 
 export default class ComingSoon extends Component {
     render() {
         return (
             <div style={{height:'100vh', backgroundColor:'#f9f9f9', flexDirection:'row',display:'flex',justifyContent:'center', alignItems:'center'}}>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Angel Foundation - Coming Soon</title>
+                    <link rel="canonical" href="https://www.angelfoundation.africa/" />
+                </Helmet>
                 <div style={{textAlign:'center'}}>
                     <img alt="angle foundation logo" src={Logo} style={{width:250}}/>
                     <h1 style={{textAlign:'center', fontSize:isMobile ? 40: 80, fontWeight:900, marginTop:10}}>Coming Soon</h1>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
 
 import React, { Component } from 'react'
+import {Helmet} from "react-helmet";
 
 class HomePage extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Angel Foundation - Home</title>
+                    <link rel="canonical" href="https://www.angelfoundation.africa/" />
+                </Helmet>
                 <h1>Home Page</h1>
             </div>
         )
