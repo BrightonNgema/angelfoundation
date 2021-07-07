603a50c30c0bcfb8680cda782a5ab58df300e69c
Brighton Ngema <brightonngema@setups-MacBook-Pro.local>
2021-07-07 21:51:55 +0200

    pages

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { Home,  ComingSoon} from 'pages';
+import { Home,  ComingSoon} from '../pages';
 
 export default (
     <Router>
