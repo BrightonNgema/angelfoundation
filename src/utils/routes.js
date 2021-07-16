46af7f7368e904fbf141665c4179eb6d3b689289
Brighton <brightonnkosana@gmail.com>
2021-07-16 20:37:23 +0200

    Revert "Partners page"

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { Home,About,OurApproach,OurGoals, HowYouCanHelp,Partners, Contact, ComingSoon} from '../pages';
+import { Home,  ComingSoon} from '../pages';
 
 export default (
     <Router>
         <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-            <Route path="/our-approach" exact component={OurApproach} />
-            <Route path="/our-goals" exact component={OurGoals} />
-            <Route path="/how-you-can-help" exact component={HowYouCanHelp} />
-            <Route path="/partners" exact component={Partners} />
-            <Route path="/contact" exact component={Contact} />
-            <Route component={ComingSoon} />
+            <Route path="/" exact component={ComingSoon} />
+            <Route component={Home} />
         </Switch>
     </Router>
 );
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,9 +1,9 @@
 const colors = {
     angel_orange:"#ED9A00",
     angel_yellow:'#FADA62',
-    angel_white:"#fff",
-    angel_black:"#000",
-    angel_dark:"#333333",
+    angle_white:"#fff",
+    angle_black:"#000",
+    angle_dark:"#333333",
 }
 
 
